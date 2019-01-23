22faea52ae03e670fd24ed0be9257956aaccb84a
Musti132 <44943708+Musti132@users.noreply.github.com>
2019-01-23 08:25:52 +0100

    Update config.php

--- config.php
+++ config.php
@@ -12,9 +12,9 @@ $setting['PASSWORD'] = '';
 $setting['COLUMN'] = 'emails';
 $setting['TABLE'] = 'emails';
 
-$setting['SMTP_HOST'] = 'smtp.mailgun.org';
-$setting['SMTP_USER'] = 'postmaster@mail.appla-user61212-authentication.com';
-$setting['SMTP_PASSWORD'] = "ee7902c7c0a176633176893746980462-060550c6-ef68e005";
+$setting['SMTP_HOST'] = 'smtp.gmail.com';
+$setting['SMTP_USER'] = '';
+$setting['SMTP_PASSWORD'] = "";
 $setting['SMTP_PORT'] = 587;
 $setting['FRA_NAVN'] = 'Firma navn';
-$setting['FRA_EMAIL'] = 'postmaster@mail.appla-user61212-authentication.com';
+$setting['FRA_EMAIL'] = 'blblabla@bla.com';
